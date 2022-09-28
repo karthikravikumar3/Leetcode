9d5a5fe6c7ef67b6a60bef3550c432dfaf73732f
karthikravikumar6 <113553044+karthikravikumar6@users.noreply.github.com>
2022-09-28 14:16:48 -0700

    Time: 36 ms (31.69%), Space: 18 MB (7.43%) - LeetHub

---
+++ 57-insert-interval/57-insert-interval.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> sol;
+        vector<pair<int,int>> pp;
+        for(auto x:intervals){
+            if((newInterval[0]>=x[0] && newInterval[0]<=x[1])||(newInterval[1]>=x[0] && newInterval[1]<=x[1]) ||(newInterval[1]>=x[1] && newInterval[0]<=x[0])){
+                pp.push_back(make_pair(x[0],x[1]));
+            }else{
+                vector<int> v;
+                v.push_back(x[0]);
+                v.push_back(x[1]);
+                sol.push_back(v);
+                
+            }
+        }
+        pp.push_back(make_pair(newInterval[0],newInterval[1]));
+        int m1=newInterval[0];
+        int m2=newInterval[1];
+        for(auto x:pp){
+            m1=min(x.first,m1);
+            m2=max(x.second,m2);
+            //cout<<x.first<<"-"<<x.second<<endl;
+        }
+        
+        vector<int> q;
+        q.push_back(m1);
+        q.push_back(m2);
+        sol.push_back(q);
+        sort(sol.begin(),sol.end());
+        return sol;
+    }
+};
