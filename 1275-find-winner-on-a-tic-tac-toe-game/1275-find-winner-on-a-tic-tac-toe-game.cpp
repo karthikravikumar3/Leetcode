235c9cd59bee1450c088c66846cf1e2eb08945d9
karthikravikumar6 <113553044+karthikravikumar6@users.noreply.github.com>
2022-10-27 17:13:32 -0700

    Time: 8 ms (17.00%), Space: 8.5 MB (19.14%) - LeetHub

--- 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.cpp
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.cpp
@@ -16,30 +16,25 @@ public:
             }
             
         }
-        for(auto x:v){
-            for(auto y:x){
-                cout<<y;
-            }
-            cout<<endl;
-        }
+      
        
         if(v[0][0]==v[1][1] && v[1][1]==v[2][2] &&v[1][1]!=-1)return v[1][1]==1?"A":"B";
-        cout<<"9";
+        //cout<<"9";
         if(v[0][2]==v[1][1] && v[1][1]==v[2][0] &&v[1][1]!=-1)return v[1][1]==1?"A":"B";
-        cout<<"1";
+        //cout<<"1";
         for(int i=0;i<3;i++){
             if(count(v[i].begin(),v[i].end(),1)==3)return "A";
             if(count(v[i].begin(),v[i].end(),0)==3)return  "B";
         }
-        cout<<"12";
+        //cout<<"12";
         
         for(int i=0;i<3;i++){
-            cout<<"hi-"<<v[0][i]<<v[1][i]<<v[2][i];
+            //cout<<"hi-"<<v[0][i]<<v[1][i]<<v[2][i];
             if(v[0][i]==v[1][i]&& v[1][i]==v[2][i] &&v[1][i]!=-1){
-                 cout<<"i-"<<i<<endl;
+                 //cout<<"i-"<<i<<endl;
                 return v[1][i]==1?"A":"B";}
         }
-        cout<<"3";
+        //cout<<"3";
         if(flag==false){
             return "Pending";
         }else{
